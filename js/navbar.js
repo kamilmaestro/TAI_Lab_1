e5661b29e6c3ce7b0b27b5cce6e763249d238a95
kamilmaestro <ksdekamil@gmail.com>
2020-03-16 19:55:48 +0100

    navbar moved to component

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+const navbar = `
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" href="index.html">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="quiz.html">QUIZ</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="contact.html">KONTAKT</a>
+        </li>
+    </ul>
+`;
+
+const navbarElement = document.querySelector(".navbar");
+navbarElement.innerHTML = navbar;
+
+
